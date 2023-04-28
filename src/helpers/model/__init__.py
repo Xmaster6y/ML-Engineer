c5dd7acbb97e3a32d795309b41b31a341518eb8f
Xmaster6y <yoann.poupart@ens-lyon.fr>
2023-04-28 17:43:15 +0200

    pytest hook and local imports

--- src/helpers/model/__init__.py
+++ src/helpers/model/__init__.py
@@ -1,5 +1,11 @@
 """
-Classes imports
+This module contains the model helpers.
+
+Submodules
+----------
+eval
+    Contains helper functions for evaluating the model.
 """
 
+from . import eval
 from .eval import Evaluator
---
+++ src/helpers/plot/__init__.py
@@ -0,0 +1,10 @@
+"""
+Plot helper functions.
+
+Submodules
+----------
+pca
+    Contains helper functions for plotting PCA.
+"""
+
+from . import pca
